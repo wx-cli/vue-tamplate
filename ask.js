3d94307a9206386c05bb532c0cd894c701858991
wangxh <wangxh@finstone.com.cn>
2021-03-24 16:18:44 +0800

    修改配置文件

---
+++ ask.js
@@ -0,0 +1,29 @@
+/*
+ * @Autor: 王向会
+ * @Date: 2021-03-24 16:12:56
+ * @LastEditors: 王向会
+ * @LastEditTime: 2021-03-24 16:13:16
+ * @Description: 
+ */
+module.exports = [
+  {
+    type: 'confirm',
+    name: 'private',
+    message: 'ths resgistery is private?',
+  },
+  {
+    type: 'input',
+    name: 'author',
+    message: 'author?',
+  },
+  {
+    type: 'input',
+    name: 'description',
+    message: 'description?',
+  },
+  {
+    type: 'input',
+    name: 'license',
+    message: 'license?',
+  },
+]
